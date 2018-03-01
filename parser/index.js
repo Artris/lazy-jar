9c284ae77a820dc9d7ac4b90e512574e44f6f385
Alireza Alidousti <alireza.eva.u23@gmail.com>
2018-02-28 22:42:39 -0800

    surfaces parsers in the root parser

--- parser/index.js
+++ parser/index.js
@@ -1,6 +1,9 @@
 const { split } = require('./helpers');
 const { parseScheduleMessage } = require('./schedule');
 const { parseAddMessage } = require('./add');
+const { parseRemoveMessage } = require('./remove');
+const { parseSkipMessage } = require('./skip');
+const { parsePaidMessage } = require('./paid');
 
 function parseCommand(command, { myUsername }) {
   const [type, message] = split(command);
@@ -9,7 +12,13 @@ function parseCommand(command, { myUsername }) {
       return parseScheduleMessage(message);
     case 'add':
       return parseAddMessage(message, myUsername);
+    case 'remove':
+      return parseRemoveMessage(message, myUsername);
     default:
+      const isPaidCommand = command.indexOf(' paid ') !== -1;
+      const isSkipCommand = command.indexOf(' will skip ') !== -1;
+      if (isPaidCommand) return parsePaidMessage(command, myUsername);
+      if (isSkipCommand) return parseSkipMessage(command, myUsername);
       throw Error('unkown command');
   }
 }
--- test/parser/index.js
+++ test/parser/index.js
@@ -18,7 +18,56 @@ describe('parseCommand', function() {
     assert.deepEqual(result, expected);
   });
 
-  it('should return an empty Object if the command is unknown', function() {
+  it('should parse add command', function() {
+    const message = 'add @dtoki to artris';
+    const expected = {
+      type: 'add',
+      to: 'artris',
+      username: '@dtoki'
+    };
+    const result = parseCommand(message, { myUsername: '@alireza.eva.u23' });
+    assert.deepEqual(result, expected);
+  });
+
+  it('should parse remove command', function() {
+    const message = 'remove @dtoki from artris';
+    const expected = {
+      type: 'remove',
+      from: 'artris',
+      username: '@dtoki'
+    };
+    const result = parseCommand(message, { myUsername: '@alireza.eva.u23' });
+    assert.deepEqual(result, expected);
+  });
+
+  it('should parse paid command', function() {
+    const message = 'I paid @dtoki $10';
+    const expected = {
+      type: 'paid',
+      to: '@dtoki',
+      from: '@alireza.eva.u23',
+      some: '$10'
+    };
+    const result = parseCommand(message, { myUsername: '@alireza.eva.u23' });
+    assert.deepEqual(result, expected);
+  });
+
+  it('should parse skip command', function() {
+    const message = 'I will skip artris for 2 months';
+    const expected = {
+      type: 'skip',
+      username: '@alireza.eva.u23',
+      name: 'artris',
+      when: {
+        count: '2',
+        period: 'month'
+      }
+    };
+    const result = parseCommand(message, { myUsername: '@alireza.eva.u23' });
+    assert.deepEqual(result, expected);
+  });
+
+  it('should throw if the command is unknown', function() {
     const message =
       'UnkownCommand artris with @alireza.eva.u23 everyday at 6am';
     assert.throws(() => {
