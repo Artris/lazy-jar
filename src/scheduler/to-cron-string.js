6df24bd8537bb1c7cf25dfb7189236fafe864789
Alireza Alidousti <alireza.eva.u23@gmail.com>
2018-03-18 14:43:55 -0700

    adds support for mapping between freq and time to cron strings

---
+++ src/scheduler/to-cron-string.js
@@ -0,0 +1,32 @@
+const frequencyToCronStringMap = new Map([
+  ['EVEYDAY', '*'],
+  ['WEEKDAYS', '1-5'],
+  ['WEEKENDS', '6,7'],
+  ['MONDAYS', '1'],
+  ['TUESDAYS', '2'],
+  ['WEDNESDAYS', '3'],
+  ['THURSDAYS', '4'],
+  ['FRIDAYS', '5'],
+  ['SATURDAYS', '6'],
+  ['SUNDAYS', '7']
+]);
+
+const freqToCronString = frequency => {
+  const isValidFrequency = frequencyToCronStringMap.has(frequency);
+  if (isValidFrequency) {
+    return frequencyToCronStringMap.get(frequency);
+  } else {
+    throw Error(`Invalid Frequency: ${frequency}`);
+  }
+};
+
+const timeToCronString = time => {
+  const { hh, mm } = time;
+  return `${mm} ${hh}`;
+};
+
+module.exports = ({ frequency, time }) => {
+  const mmhh = timeToCronString(time);
+  const dayOfWeek = freqToCronString(frequency);
+  return `* ${mmhh} * * ${dayOfWeek}`;
+};
