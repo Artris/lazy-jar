9d12034f3b8f174044ddad1aad49239cfd179574
Dolapo <dolapotoki@gmail.com>
2018-04-01 01:01:47 +0000

    Refactores helpers.factory.js

--- src/database/helpers/helpers.factory.js
+++ src/database/helpers/helpers.factory.js
@@ -4,77 +4,55 @@ module.exports = ({
     Notification,
     Log
 }) => {
-    const getState = (data) => State.find(data); 
     return {
-        saveSecrets: ({
+        saveSecrets,
+        returnSecrets,
+        saveState,
+        returnEventsByTeamId,
+        returnState
+    }
+    
+    function saveSecrets({team_id, access_token, bot_user_id, bot_access_token}) {
+        const data = {
             team_id,
             access_token,
-            bot_user_id,
-            bot_access_token
-        }) => {
-            const data = {
-                team_id,
-                access_token,
-                bot: {
-                    bot_user_id,
-                    bot_access_token
-                }
+            bot: {
+                bot_user_id,
+                bot_access_token
             }
-            return new Promise((resolve, reject) => {
-                Secret.findOneAndUpdate({
-                    team_id
-                }, data, {
-                    'new': true,
-                    upsert: true
-                }, (err, doc) => {
-                    if (err) reject(err);
-                    resolve(doc);
-                })
-            });
-        },
-        returnSecrets: ({
-            team_id
-        }) => {
-            return Secret.find({
-                team_id
-            });
-        },
-        saveState: (data) => {
-            const {
-                team_id,
-                event_id
-            } = data
-            return new Promise((resolve, reject) => {
-                State.findOneAndUpdate({
-                    team_id,
-                    event_id
-                }, data, {
-                    'new': true,
-                    upsert: true
-                }, (err, doc) => {
-                    if (err) reject(err);
-                    resolve(doc);
-                });
-            });
-        },
-        returnEventsByTeamId: (data) => {
-            return new Promise((resolve, reject) => {
-                getState(data).then(result => {
-                        resolve(result.map(data => data.event_id));
-                    })
-                    .catch(err => {
-                        reject(err);
-                    });
-            });
-        },
-        returnState: (data) => {
-            return new Promise((resolve, reject) => {
-                getState(data).then(result => {
-                    resolve(result)
-                }).catch(err => {
-                    reject(err);
-                })
-            })
         }
+        
+        return Secret.findOneAndUpdate({ team_id }, data, {
+            'new': true,
+            upsert: true
+        })
+    }
+
+    function returnSecrets({team_id}){
+        return Secret.find({ team_id });
+    }
+    
+    function saveState(data){
+        const {
+            team_id,
+            event_id
+        } = data
+        
+        return State.findOneAndUpdate({
+            team_id,
+            event_id
+            }, data, {
+                'new': true,
+                upsert: true
+            }
+        );
+    }
+    
+    function returnEventsByTeamId(data){
+        return State.find(data).then(res => res.map(data => data.event_id))
+    }
+    
+    function returnState(data){
+        return State.find(data);
     }
 }
