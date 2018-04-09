a1cff7a3293b29832403468c4434c2e7c9aae3ea
Alireza Alidousti <alireza.eva.u23@gmail.com>
2018-04-08 18:09:08 -0700

    removes command helpers and cleans app.js

--- src/app.js
+++ src/app.js
@@ -43,22 +43,7 @@ const {
 const redirect_uri = `${host}:${port}/oauth/redirect`;
 const parser = require('./parser/index');
 const createAction = require('./actions/index');
-const lazyJar = require('./reducers/index');
-
-const {
-  getTeamEventsSet,
-  interpretCommand,
-  getPreviousState,
-  updateState
-} = require('./app-helpers/command')(
-  getEventsFor,
-  parser,
-  createAction,
-  getState,
-  lazyJar,
-  saveState,
-  winston
-);
+const reduce = require('./reducers/index');
 
 const Job = require('./scheduler/job.factory')(
   getState,
@@ -96,7 +81,6 @@ app.get('/oauth/authorize', (req, res) => {
     pathname: slack_auth_uri,
     query: params
   });
-  winston.info(`Oauth authourization request made, redirecting to slack`);
   res.redirect(auth_url);
 });
 
@@ -104,10 +88,9 @@ app.get('/oauth/redirect', async (req, res) => {
   const { code, state } = req.query;
   try {
     await getSecretsAndSave(code);
-    winston.info(`Team authencation successful`);
     res.send('Thank you, you have successfully authenticated your team!');
-  } catch (e) {
-    winston.error(`Team authencation failed, ${e}`);
+  } catch (err) {
+    winston.error(`Team authencation failed, ${err}`);
     res.send(
       'Oops, an error occured while authenticating your team, please try again!'
     );
@@ -115,37 +98,41 @@ app.get('/oauth/redirect', async (req, res) => {
 });
 
 app.post('/api/command', (req, res) => {
-  command(req.body)
-    .then(() => {
-      res.send('State updated');
-    })
+  executeCommand(req.body)
+    .then(() => res.send('State updated'))
     .catch(err => {
       res.send('Something is wrong, please try again later');
       console.log(err);
     });
 });
 
-async function command({ team_id, user_id, text }) {
+app.listen(port, () => console.log(`listening on port ${port}!`));
+
+async function executeCommand({ team_id, user_id, text }) {
   const secret = await getSecret({ team_id });
-  const teamEvents = await getTeamEventsSet(team_id);
+  const events = await getEventsFor({ team_id });
+  const eventIds = new Set(events.map(e => e.event_id));
 
-  const usersInfo = await getUsersInfo(secret.bot.bot_access_token);
-  const action = await interpretCommand(text, usersInfo, user_id, teamEvents);
-  const prevState = await getPreviousState(team_id, action.event);
-  const updatedState = await updateState(action, prevState, team_id);
+  const bot_access_token = secret.bot.bot_access_token;
+  const usersInfo = await getUsersInfo(bot_access_token);
+  const command = parser(text);
+  const action = createAction(command, usersInfo, user_id, eventIds, 'UTC');
+  const currState = await getState(team_id, action.event);
+  const nextState = await reduce(action, currState);
+  // TODO: a new state out of the reducer should already include the team_id
+  nextState.team_id = team_id;
 
+  await saveState(nextState);
   switch (action.type) {
     case 'SCHEDULE':
-      scheduler.add([updatedState]);
+      scheduler.add([nextState]);
       break;
     case 'HALT':
     case 'TERMINATE':
-      scheduler.cancel([updatedState]);
+      scheduler.cancel([nextState]);
       break;
     case 'MOVE':
-      scheduler.reschedule([updatedState]);
+      scheduler.reschedule([nextState]);
       break;
   }
 }
-
-app.listen(port, () => console.log(`listening on port ${port}!`));
