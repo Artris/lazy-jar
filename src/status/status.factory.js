ba8bb8c79d7aaa929a010d73269b3148305d079a
yasster <ymissoum1@gmail.com>
2018-05-18 14:50:14 -0700

    updated status

--- src/status/status.factory.js
+++ src/status/status.factory.js
@@ -9,6 +9,13 @@ module.exports = (logProvider, logger) => {
   function getAllMemberIds(notifications) {
     return [...new Set(notifications.map(n => n.user_id))];
   }
+<<<<<<< HEAD
+=======
+
+  function getAllEventIds(events) {
+    return [...new Set(events.map(n => n.event_id))];
+  }
+>>>>>>> 2872545... Updated Status
 
   function initializeMemberStatus(memberIds) {
     return new Map(
@@ -23,6 +30,22 @@ module.exports = (logProvider, logger) => {
     );
   }
 
+<<<<<<< HEAD
+=======
+  function initializeEventStatus(eventIds) {
+    return new Map(
+      eventIds.map(event_id => [
+        event_id,
+        {
+          event_id,
+          notified: 0,
+          participated: 0
+        }
+      ])
+    );
+  }
+
+>>>>>>> 2872545... Updated Status
   function allTimeMemberStatus(notifications) {
     const memberIds = getAllMemberIds(notifications);
     const memeberStatus = initializeMemberStatus(memberIds);
@@ -36,6 +59,7 @@ module.exports = (logProvider, logger) => {
 
   return team_id =>
     logProvider({ team_id })
+<<<<<<< HEAD
       .then(notifications => allTimeMemberStatus(notifications))
       .catch(err => {
         console.log(err);
@@ -49,4 +73,23 @@ module.exports = (logProvider, logger) => {
         });
         throw err;
       });
+=======
+    .then(notifications => {
+      const eventStatus = eventMemberStatus(notifications)
+      const memberStatus = allTimeMemberStatus(notifications)
+      return { eventStatus, memberStatus }
+    })
+    .catch(err => {
+      console.log(err);
+      logger.log({
+        level: 'error',
+        message: stripIndent `
+                        failed to calculate the status for
+                        team_id:  ${team_id}
+                        error:    ${err}
+                        `
+      });
+      throw err;
+    });
+>>>>>>> 2872545... Updated Status
 };
