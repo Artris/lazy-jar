48b81a362f6acb38e5885f9101d2889f94d934d6
= <graziettahof@gmail.com>
2018-05-20 18:51:25 +0000

    adds meaure for weekly and monthly status

--- src/status/formatter.js
+++ src/status/formatter.js
@@ -1,14 +1,33 @@
-module.exports = (info, userIdToUsername) => {
-  return [...info.values()]
+module.exports = ({ eventStatus, memberStatus }, userIdToUsername) => {
+  console.log(memberStatus);
+  
+  return [...memberStatus.values()]
     .filter(member => userIdToUsername.has(member.id))
     .sort((a, b) => a.participated / a.notified >= b.participated / b.notified)
     .map(member => {
       const name = userIdToUsername.get(member.id);
       const missed = member.notified - member.participated;
+      const meetingsMissedCurrentMonth = member.notifiedCurrentMonth - member.participatedCurrentMonth
+      const meetingsMissedCurrentWeek = member.notifiedCurrentWeek - member.participatedCurrentWeek
       const participation_rate = Math.floor(
         member.participated * 100 / member.notified
       );
-      return `${name} has missed ${missed} meetings, ${participation_rate}% participation rate`;
+      return `${name} has missed:
+        This month: ${meetingsMissedCurrentMonth} meetings
+        This week:  ${meetingsMissedCurrentWeek} meetings
+        In total: ${missed} meetings
+        Participation rate: ${participation_rate}%\n`;
     })
-    .join('\n');
+    .concat(
+      [...eventStatus.values()]
+      .sort((a, b) => a.participated / a.notified >= b.participated / b.notified)
+      .map(event => {
+        const event_name = event.event_id;
+        const event_participation_rate = Math.floor(
+        event.participated * 100 / event.notified
+        );
+        return `${event_name} has a ${event_participation_rate}% participation rate.`;
+      })
+      )
+    .join('\n')
 };
--- src/status/status.factory.js
+++ src/status/status.factory.js
@@ -1,10 +1,31 @@
 const { stripIndent } = require('common-tags');
+const moment = require('moment');
 
 /**
  * Generates a simple status message based the Logs
  * @param {(team_id) -> Notification list} logProvider
  * @param logger responds to `.log` method
  */
+ 
+ /*http://momentjscom.readthedocs.io/en/latest/moment/07-customization/11-calendar-format/*/
+ function occuredWithinCurrentWeek(date) {
+      /*TODO: add timezone*/
+    const now = moment();
+    const then = moment(date, "YYYYMMDD");
+    
+    
+    return now.format("W") === then.format("W");
+ }
+ 
+ function occuredWithinCurrentMonth(date) {
+         /*TODO: add timezone*/
+    const now = moment();
+    const then = moment(date, "YYYYMMDD");
+    return (now.format("M") === then.format("M")) &&
+    (now.format("YY") === then.format("YY"));
+
+ }
+ 
 module.exports = (logProvider, logger) => {
   function getAllMemberIds(notifications) {
     return [...new Set(notifications.map(n => n.user_id))];
@@ -21,7 +42,11 @@ module.exports = (logProvider, logger) => {
         {
           id,
           notified: 0,
-          participated: 0
+          participated: 0,
+          notifiedCurrentWeek: 0,
+          participatedCurrentWeek: 0,
+          notifiedCurrentMonth: 0,
+          participatedCurrentMonth: 0
         }
       ])
     );
@@ -42,13 +67,17 @@ module.exports = (logProvider, logger) => {
 
   function allTimeMemberStatus(notifications) {
     const memberIds = getAllMemberIds(notifications);
-    const memeberStatus = initializeMemberStatus(memberIds);
-    notifications.forEach(({ user_id, action }) => {
-      const status = memeberStatus.get(user_id);
+    const memberStatus = initializeMemberStatus(memberIds);
+    notifications.forEach(({ user_id, action, date }) => {
+      const status = memberStatus.get(user_id);
       if (action === 'Participated') status.participated += 1;
       if (action === 'Notified') status.notified += 1;
+      if (occuredWithinCurrentWeek(date)  && action === 'Participated') status.participatedCurrentWeek += 1;
+      if (occuredWithinCurrentWeek(date)  && action === 'Notified') status.notifiedCurrentWeek += 1;
+      if (occuredWithinCurrentMonth(date) && action === 'Participated') status.participatedCurrentMonth += 1;
+      if (occuredWithinCurrentMonth(date) && action === 'Notified') status.notifiedCurrentMonth += 1;
     });
-    return memeberStatus;
+    return memberStatus;
   }
 
   function allTimeEventStatus(events) {
@@ -65,6 +94,7 @@ module.exports = (logProvider, logger) => {
   return team_id =>
     logProvider({ team_id })
     .then(notifications => {
+      console.log(notifications)
       const eventStatus = allTimeEventStatus(notifications)
       const memberStatus = allTimeMemberStatus(notifications)
       return { eventStatus, memberStatus }
