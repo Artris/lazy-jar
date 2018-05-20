60417f1e79b0bbe1eb47dee9597846caf909fad0
Alireza Alidousti <alireza.eva.u23@gmail.com>
2018-05-19 21:49:56 -0400

    logs participation notification

--- src/app.js
+++ src/app.js
@@ -116,7 +116,9 @@ app.post('/api/command', (req, res) => {
 app.post('/api/notifications', async (req, res) => {
   const payload = JSON.parse(req.body.payload),
     notification = payload.actions.find(e => e.name === 'notification').value,
-    [team_id, event_id, fire_date, user_id] = notification.split(',');
+    [team_id, event_id, date, user_id] = notification.split(',');
+
+  saveLog({ team_id, event_id, user_id, date, action: 'participated' });
 
   const secret = await getSecret({ team_id }),
     token = secret.bot.bot_access_token;
