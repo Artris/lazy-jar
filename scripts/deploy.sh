507d9a21be656379ae8d85aa352b001e80dd225c
Dolapo Toki <dolapotoki@gmail.com>
2018-06-04 23:10:27 -0700

    adds continuous delivery to google cloud registory

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+docker build -t gcr.io/production-artris/mongo ../mongo/
+docker build -t gcr.io/production-artris/app ../app/
+
+gcloud auth activate-service-account --key-file deploy.key.json
+yes | gcloud auth configure-docker
+
+docker push gcr.io/production-artris/mongo:latest
+docker push gcr.io/production-artris/app:latest
