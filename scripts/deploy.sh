c29069c7fd7287e36f50ced30c654ab77c8c58f8
Dolapo Toki <dolapotoki@gmail.com>
2018-06-30 14:25:10 -0700

    add's staging and master seprate build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,14 @@
 
 set -e
 
-docker build -t gcr.io/production-artris/mongo ../mongo/
-docker build -t gcr.io/production-artris/app ../app/
+DEPLOY_ENV="$1"
+VERSION="$2"
+
+docker build -t gcr.io/production-artris/mongo:$DEPLOY_ENV-$VERSION ../mongo/
+docker build -t gcr.io/production-artris/app:$DEPLOY_ENV-$VERSION ../app/
 
 gcloud auth activate-service-account --key-file ${TRAVIS_BUILD_DIR}/deploy.key.json
 yes | gcloud auth configure-docker
 
-docker push gcr.io/production-artris/mongo:latest
-docker push gcr.io/production-artris/app:latest
+docker push gcr.io/production-artris/mongo:$DEPLOY_ENV-$VERSION
+docker push gcr.io/production-artris/app:$DEPLOY_ENV-$VERSION
